4e76d408dd160d8e29e60f7425bcdcaf49dbf326
parshant <parshantbisht656@gmail.com>
2026-05-16 19:25:55 +0530

    final correct path

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,29 +5,22 @@ import path from "path";
 const app = express();
 const __dirname = path.resolve();
 
-// API route
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is healthy" });
 });
 
-// API route
 app.get("/books", (req, res) => {
   res.status(200).json({ message: "this is a list of books" });
 });
 
-// Production mode me frontend build serve karo
-// express.static() CSS, JS, images ko serve karega
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+const frontendPath = path.join(__dirname, "..", "frontend", "dist");
 
-  // React Router ke liye catch-all route
-  // Ye tab chalega jab koi API route match nahi hoga
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
-  });
-}
+app.use(express.static(frontendPath));
 
-// Server start
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
+app.get("/{*app}", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+app.listen(ENV.PORT || 3000, () => {
+  console.log(`Server is running on port ${ENV.PORT || 3000}`);
 });
