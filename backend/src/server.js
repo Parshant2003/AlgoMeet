2cc22867301ba169c6b68a0fed1be155af270ae9
parshant <parshantbisht656@gmail.com>
2026-05-19 19:37:24 +0530

    stream,sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,10 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js";
+
 
 const app = express();
 
@@ -14,9 +18,10 @@ const app = express();
 app.use(express.json());
 // servers 
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-
+app.use("/api/chat", chatRoutes);
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -26,6 +31,7 @@ const frontendPath = path.resolve(__dirname, "../../frontend/dist");
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
+
 
 app.use(express.static(frontendPath));
 
